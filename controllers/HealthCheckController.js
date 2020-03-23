e7fb41d7fabb84983cb9ad161fb1453a6b7966a5
Fixxy <kewell5@live.com>
2020-03-24 00:10:25 +0700

    add timpstamp function

--- controllers/HealthCheckController.js
+++ controllers/HealthCheckController.js
@@ -4,7 +4,7 @@ var router = express.Router();
 router.get("/", async (req, res) => {
   const healthcheck = {
     status: "Healthy",
-    totalDuration: process.uptime(),
+    totalDuration: secondsToHms(process.uptime()),
     entries: {},
     timestamp: Date.now()
   };
@@ -15,5 +15,23 @@ router.get("/", async (req, res) => {
     res.status(503).json(healthcheck);
   }
 });
+
+function secondsToHms(num) {
+  var sec_num = parseInt(num, 10); // don't forget the second param
+  var hours = Math.floor(sec_num / 3600);
+  var minutes = Math.floor((sec_num - hours * 3600) / 60);
+  var seconds = sec_num - hours * 3600 - minutes * 60;
+
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  return hours + ":" + minutes + ":" + seconds;
+}
 
 module.exports = router;
